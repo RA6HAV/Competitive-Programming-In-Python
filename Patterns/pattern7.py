4f32add06d4ddb1f282de0b4b2f5aebc33d6cc9c
Raghav Chaudhary <ra6hav021@gmail.com>
2025-09-01 08:51:59 +0530

    Add files via upload

---
+++ Patterns/pattern7.py
@@ -0,0 +1,12 @@
+# Hollow pyramid pattern
+
+n= 5
+
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(2*n-2*i):
+        print(" ",end=" ")
+    for j in range(i):
+            print("*",end=" ")
+    print()
